9a53559452c7dbf84b7de6eb81184e712f582eb4
Andrii Rybalko <rybalko.andriy@gmail.com>
2021-03-08 00:58:40 +0200

    implemented test first realization

---
+++ src/YoutubeMusicBot/BotHostedService.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Telegram.Bot;
+
+[assembly: InternalsVisibleTo("YoutubeMusicBot.Tests")]
+
+namespace YoutubeMusicBot
+{
+	internal class BotHostedService : IHostedService
+	{
+		private readonly ITelegramBotClient _client;
+
+		public BotHostedService(
+			ITelegramBotClient client)
+		{
+			_client = client;
+		}
+
+		public async Task StartAsync(CancellationToken cancellationToken)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public async Task StopAsync(CancellationToken cancellationToken)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
--- tests/YoutubeMusicBot.Tests/BotTests.cs
+++ tests/YoutubeMusicBot.Tests/BotTests.cs
@@ -1,18 +1,67 @@
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoFixture;
+using Moq;
 using NUnit.Framework;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.InputFiles;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace YoutubeMusicBot.Tests
 {
 	public class BotTests
 	{
+		private Fixture _fixture = null!;
+
 		[SetUp]
 		public void Setup()
 		{
+			_fixture = new Fixture();
+			_fixture.Behaviors.OfType<ThrowingRecursionBehavior>()
+				.ToList()
+				.ForEach(b => _fixture.Behaviors.Remove(b));
+			_fixture.Behaviors.Add(new OmitOnRecursionBehavior());
 		}
 
 		[Test]
-		public void ShouldUploadAudioOnEcho()
+		[TestCase("https://youtu.be/wuROIJ0tRPU")]
+		public async Task ShouldUploadAudioOnEcho(string url)
 		{
+			var clientMock = new Mock<ITelegramBotClient>();
+			var botHostedService = new BotHostedService(clientMock.Object);
+			await botHostedService.StartAsync(CancellationToken.None);
+			var message = _fixture
+				.Build<Message>()
+				.With(m => m.Text, url)
+				.Create();
 
+			clientMock.Raise(
+				c => c.OnMessage += null,
+				this,
+				new MessageEventArgs(message));
+
+			clientMock.Verify(
+				c => c.SendAudioAsync(
+					message.Chat.Id,
+					It.Is<InputOnlineFile>(
+						f => f.FileType == FileType.Stream
+							&& f.Content != null
+							&& f.Content.Length > 0),
+					It.IsAny<string>(),
+					It.IsAny<ParseMode>(),
+					It.IsAny<int>(),
+					It.IsAny<string>(),
+					It.IsAny<string>(),
+					It.IsAny<bool>(),
+					It.IsAny<int>(),
+					It.IsAny<IReplyMarkup>(),
+					It.IsAny<CancellationToken>(),
+					It.IsAny<InputMedia>()));
 		}
 	}
 }
